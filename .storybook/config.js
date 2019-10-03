5d676ac73d3b7644fc2ae3ad8cd10a94385337e0
Bede Kelly <me@bede.io>
2019-10-03 14:02:14 +0100

    Add responsive viewports to storybook config and fix alignment of retweet info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
+
+addParameters({
+    viewport: {
+	viewports: INITIAL_VIEWPORTS
+    }
+})
